1b1f2af27ea23b729436e142194cef871b204ca2
kurnia <mkurniap91@gmail.com>
2025-05-22 21:25:42 +0700

    add terraform

---
+++ terraform/gar.tf
@@ -0,0 +1,11 @@
+resource "google_artifact_registry_repository" "ntx-repo" {
+  location      = var.region
+  repository_id = var.repo_id
+  format        = var.repo_format
+}
+
+data "google_artifact_registry_docker_image" "ntx-image" {
+  location      = google_artifact_registry_repository.ntx-repo.location
+  repository_id = google_artifact_registry_repository.ntx-repo.repository_id
+  image_name    = "var.image_name:var.image_tag"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "project" {
+    type= string
+    description = "ID Google project"
+    default = "ntx-devops"
+}
+
+variable "region" {
+    type= string
+    description = "Region Google project"
+    default = "asia-southeast1"
+}
+
+variable "repo_id" {
+    type = string
+    description = "repository name"
+    default = "ntx-repo"
+}
+
+variable "repo_format" {
+    type = string
+    description = "docker registry"
+    default = "DOCKER"
+}
+
+variable "image_name" {
+    type = string
+    description = "image name"
+    default = "ntx-image"
+}
+
+variable "image_tag" {
+    type = string
+    description = "image tag"
+    default = "latest"
+}
