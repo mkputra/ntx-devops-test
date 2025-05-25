3643a3973292bb0cbd6006e5012c69c7375e24b2
kurnia <mkurniap91@gmail.com>
2025-05-25 19:42:29 +0700

    update workflow terraform

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -5,5 +5,5 @@ resource "google_artifact_registry_repository" "ntx-repo" {
 }
 
 output "artifact_registry_url" {
-  value = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_name}"
+  value = "${var.region}-docker.pkg.dev/${var.project_id}/${var.repo_id}"
 }
