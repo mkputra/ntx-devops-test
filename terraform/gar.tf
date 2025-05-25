81bddff7124e00cd4019c1970e31c1c7194a5c49
kurnia <mkurniap91@gmail.com>
2025-05-25 19:32:28 +0700

    update workflow terraform

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -4,7 +4,7 @@ resource "google_artifact_registry_repository" "ntx-repo" {
   format        = var.repo_format
 }
 
-data "google_artifact_registry_docker_image" "ntx-image" {
+data "google_artifact_registry_docker_image" "ntx-img-app" {
   location      = google_artifact_registry_repository.ntx-repo.location
   repository_id = google_artifact_registry_repository.ntx-repo.repository_id
   image_name    = "${var.image_name}:${var.image_tag}"
